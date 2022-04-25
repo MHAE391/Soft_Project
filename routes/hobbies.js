d965bdebad2f56f77a7d1866b0ae24bb66f86aba
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-04-25 15:41:28 +0200

    Refactored the code and applied single responsibility.

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -1,59 +1,7 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+const { HobbyEntity, validate } = require("../models/hobby");
 const express = require("express");
 
 const router = express.Router();
-
-//I did not add every validator just for simplicity
-const HobbyEntity = mongoose.model(
-  "Hobby",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 3,
-    },
-    description: {
-      type: String,
-      required: true,
-      minlength: 30,
-      maxlength: 3000,
-    },
-    sessions: {
-      type: Number,
-      required: true,
-      min: 2,
-      max: 100,
-    },
-    experienceInYears: {
-      type: Number,
-      required: true,
-    },
-    timeOfSessionInMinuts: {
-      type: Number,
-      required: true,
-      min: 10,
-    },
-    language: {
-      type: String,
-      minlength: 3,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-    },
-    place: {
-      type: String,
-      minlength: 3,
-      required: true,
-    },
-    priceInCent: {
-      type: Number,
-      required: true,
-    },
-  })
-);
 
 router.get("/", async (req, res) => {
   res.status(200).send(await HobbyEntity.find().sort("name"));
@@ -68,7 +16,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateAgainstErrors(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let hobbyEntity = new HobbyEntity({
@@ -89,7 +37,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateAgainstErrors(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const hobbyEntity = await HobbyEntity.findByIdAndUpdate(
@@ -124,25 +72,5 @@ router.delete("/:id", async (req, res) => {
 
   return res.status(204).send(hobbyEntity);
 });
-
-function validateAgainstErrors(hobbyEntity) {
-  const scheme = {
-    name: Joi.string().min(3).required(),
-    description: Joi.string().min(30).max(3000).required(),
-    sessions: Joi.number().positive().greater(0).min(2).max(100).required(),
-    experienceInYears: Joi.number().positive().greater(0).required(),
-    timeOfSessionInMinuts: Joi.number()
-      .positive()
-      .greater(0)
-      .min(10)
-      .required(),
-    language: Joi.string().min(3).required(),
-    status: Joi.string().required(), //That's should be enum by JS got no enums
-    place: Joi.string().min(3).required(),
-    priceInCent: Joi.number().positive().greater(0).required(),
-  };
-
-  return Joi.validate(hobbyEntity, scheme);
-}
 
 module.exports = router;
