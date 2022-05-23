4e76d84157a18771fd9e394f56ea64625fb89c43
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-05-23 17:30:33 +0200

    Updated Joi and added default route handler

--- models/order.js
+++ models/order.js
@@ -1,44 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
-
-const OrderSchema = new mongoose.Schema({
-    createdAt:{
-        type:Date,
-        default: new Date(),
-    },
-   
-    coachId:{
-        type:mongoose.Types.ObjectId,
-        required: true
-    },
-    learnerId:{
-        type:mongoose.Types.ObjectId,
-        required: true
-
-    },
-    hobbyId:{
-        type:mongoose.Types.ObjectId,
-        required: true
-
-    }
+const orderEntitySchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  coachId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  learnerId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  hobbyId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
 });
 
-//const OrderEntity = mongoose.model("Order",OrderSchema);
-
 function validateAgainstErrors(orderEntity) {
-    const scheme = {
-      
-      createdAt: Joi.required(),
-      coachId: Joi.string().required(),
-      learnerId: Joi.string().required(),
-      hobbyId: Joi.string().required(),
+  const scheme = {
+    hobbyId: Joi.objectId().required(),
+    coachId: Joi.objectId().required(),
+    learnerId: Joi.objectId().required(),
+  };
 
-      
-        
-    };
-  
-    return Joi.validate(orderEntity, scheme);
-  }
-exports.OrderEntity =  mongoose.model("Order",OrderSchema);; 
+  return Joi.validate(orderEntity, scheme);
+}
+
+exports.OrderEntity = mongoose.model("Order", orderEntitySchema);
 exports.validate = validateAgainstErrors;
--- routes/orders.js
+++ routes/orders.js
@@ -1,34 +1,24 @@
-const { OrderEntity,validate }= require('../models/order');
+const _ = require("lodash");
+const { OrderEntity, validate } = require("../models/order");
 
 const express = require("express");
-
 const router = express.Router();
 
-router.post("/",async(req,res)=>{
+router.post("/make-order", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  const orderEntity = new OrderEntity(
+    _.pick(req.body, ["hobbyId", "learnerId", "coachId"])
+  );
 
-    let orderEntity = new OrderEntity({
+  if (orderEntity.coachId === orderEntity.learnerId)
+    return res.status(400).send("Coach and Learner must not be same");
 
-        hobbyId: req.body.hobbyId,
-        learnerId: req.body.learnerId,
-        coachId: req.body.coachId,
-       
-    });
+  console.log(orderEntity);
+  await orderEntity.save();
 
-    if(orderEntity.coachId = orderEntity.learnerId)
-    {
-        return res.status(400).send("Coach and Learner must not be same");
-    }
-
-    console.log(orderEntity);
-    await orderEntity.save();
-
-    return res.status(201).send(orderEntity);
-   //return res.send(orderEntity);
-})
-
-
+  res.status(201).send(orderEntity);
+});
 
 module.exports = router;
